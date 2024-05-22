0d05128f9c1c7b9b49bf6f8df51690c1436b52ca
jsunn-y <jasonyang12345@gmail.com>
2024-05-22 14:26:26 -0700

    added a bunch of retrieval baselines and calculated them

--- task2_baselines/CREEP/step_02_extract_CREEP.py
+++ task2_baselines/CREEP/step_02_extract_CREEP.py
@@ -56,7 +56,7 @@ if __name__ == "__main__":
 
     parser.add_argument("--SSL_emb_dim", type=int, default=256)
     # parser.add_argument("--dataset_folder", type=str)
-    parser.add_argument("--dataset", type=str, default="easy_reaction_test", choices=["all_proteins", "easy_reaction_test", "medium_reaction_test", "hard_reaction_test"])
+    parser.add_argument("--dataset", type=str, default="easy_reaction_test")
     parser.add_argument("--modality", type=str, default="protein", choices=["protein", "reaction", "text"])
     parser.add_argument("--protein_backbone_model", type=str, default="ProtT5", choices=["ProtT5"])
     parser.add_argument("--text_backbone_model", type=str, default="SciBERT")
@@ -205,27 +205,28 @@ if __name__ == "__main__":
     output_folder = os.path.join(args.pretrained_folder, "representations")
     os.makedirs(output_folder, exist_ok=True)
 
-    saved_file_path = os.path.join(output_folder, args.dataset + "_representations")
+    if args.get_cluster_centers == False:
+    
+        saved_file_path = os.path.join(output_folder, args.dataset + "_representations")
 
-    #if the file exists, load it
-    if os.path.exists(saved_file_path + ".npy"):
-        results = np.load(saved_file_path + ".npy", allow_pickle=True).item()
-    else:
-        results = {}
+        #if the file exists, load it
+        if os.path.exists(saved_file_path + ".npy"):
+            results = np.load(saved_file_path + ".npy", allow_pickle=True).item()
+        else:
+            results = {}
 
-    #TODO: don't save all the tensors if you don't need them all
-    if args.modality == "protein":
-        results["protein_repr_array"] = repr_array
-    elif args.modality == "reaction":
-        results["reaction_repr_array"] = repr_array
-    elif args.modality == "text":
-        results["text_repr_array"] = repr_array
-        
-    np.save(saved_file_path, results)
+        #TODO: don't save all the tensors if you don't need them all
+        if args.modality == "protein":
+            results["protein_repr_array"] = repr_array
+        elif args.modality == "reaction":
+            results["reaction_repr_array"] = repr_array
+        elif args.modality == "text":
+            results["text_repr_array"] = repr_array
+            
+        np.save(saved_file_path, results)
 
     # repr_array = np.load(saved_file_path + ".npy", allow_pickle=True).item()["protein_repr_array"]
-
-    if args.get_cluster_centers:
+    else:
         df['index'] = df.index
         ec2index = df.groupby('EC number')['index'].apply(list).to_frame().to_dict()['index']
         EClist = np.loadtxt("../../processed_data/EC_list.txt", dtype=str)
@@ -235,13 +236,14 @@ if __name__ == "__main__":
 
         #temporary line to just check if the code runs
         #EClist = [ec for ec in EClist if ec in ec2index.keys()]
-        assert len(EClist) == len(ec2index.keys())
+        #assert len(EClist) == len(ec2index.keys())
         
         cluster_centers = np.zeros((len(EClist), repr_array.shape[1]))
         for i, ec in enumerate(EClist):
             #average together the embeddings for each EC number
-            indices = ec2index[ec]
-            cluster_centers[i] = np.mean(repr_array[indices], axis=0)
+            if ec in ec2index.keys():
+                indices = ec2index[ec]
+                cluster_centers[i] = np.mean(repr_array[indices], axis=0)
 
         saved_file_path = os.path.join(output_folder, "all_ECs_cluster_centers")
         #if the file exists, load it
---
+++ task2_baselines/Similarity/example.sh
@@ -0,0 +1,4 @@
+cd ..
+python downstream_retrieval.py --pretrained_folder=Similarity/output/easy_split --query_dataset=easy_reaction_test --reference_dataset=all_ECs --query_modality=reaction --reference_modality=reaction
+
+python downstream_retrieval.py --pretrained_folder=Similarity/output/medium_split --query_dataset=medium_reaction_test --reference_dataset=all_ECs --query_modality=reaction --reference_modality=reaction
